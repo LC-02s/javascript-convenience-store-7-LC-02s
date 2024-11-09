c54648ecb0a892535e26045bce1b81cb4386af03
ChanLee_KR <cl700120@gmail.com>
2024-11-09 20:37:47 +0900

    feat: 상품 타입 정의

---
+++ src/types/Product.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} Product
+ * @property {number} id
+ * @property {string} name
+ * @property {number} price
+ * @property {number} quantity
+ * @property {string | null} promotion
+ */
+
+export {};
