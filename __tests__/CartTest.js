eb9450a7f6e4ab4afcd02730e5ac2761a09bf141
ChanLee_KR <cl700120@gmail.com>
2024-11-10 01:10:28 +0900

    test: 장바구니 클래스 테스트 추가 작성

--- __tests__/CartTest.js
+++ __tests__/CartTest.js
@@ -33,4 +33,42 @@ describe('장바구니 클래스 테스트', () => {
       expect(tester).toThrow(ERROR_PREFIX);
     });
   });
+
+  test('장바구니에 상품을 중복하여 담을 경우 상품의 누계를 출력한다.', () => {
+    const testResults = { 콜라: 8, 감자칩: 2 };
+    const input =
+      '[콜라-2],[콜라-2],[감자칩-1],[콜라-2],[콜라-2],[감자칩-1]'.split(',');
+
+    const cart = new Cart(input, productDB);
+
+    const products = cart.getProducts();
+
+    products.forEach(({ name, quantity }) => {
+      expect(testResults[name]).toBe(quantity);
+    });
+  });
+
+  test('장바구니에 상품을 추가할 경우 상품의 누계를 출력한다.', () => {
+    const firstResults = { 콜라: 6, 감자칩: 1 };
+    const firstInput = '[콜라-2],[감자칩-1],[콜라-2],[콜라-2]'.split(',');
+
+    const lastResults = { 콜라: 8, 감자칩: 2 };
+    const lastInput = '[콜라-2],[감자칩-1]'.split(',');
+
+    const cart = new Cart(firstInput, productDB);
+
+    const firstProducts = cart.getProducts();
+
+    firstProducts.forEach(({ name, quantity }) => {
+      expect(firstResults[name]).toBe(quantity);
+    });
+
+    cart.addProduct(lastInput, productDB);
+
+    const lastProducts = cart.getProducts();
+
+    lastProducts.forEach(({ name, quantity }) => {
+      expect(lastResults[name]).toBe(quantity);
+    });
+  });
 });
