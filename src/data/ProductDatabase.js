c10fc9c110c4b4985a107dc507365503cf3190ad
ChanLee_KR <cl700120@gmail.com>
2024-11-09 21:22:32 +0900

    feat: 상품 데이터베이스 검색 기능 구현

--- src/data/ProductDatabase.js
+++ src/data/ProductDatabase.js
@@ -11,6 +11,21 @@ class ProductDatabase {
     const productCSV = readFileSync(DATA_PATH.PRODUCT_CSV);
     this.#data = parseCSV(productCSV);
   }
+
+  /** @param {Pick<Product, 'name'>} query */
+  findByName({ name }) {
+    return this.#data.filter((product) => product.name === name);
+  }
+
+  /** @param {Pick<Product, 'id'>} query */
+  findById({ id }) {
+    return this.#data[id];
+  }
+
+  /** @param {(data: Product[]) => void} printer */
+  printAll(printer) {
+    printer(this.#data);
+  }
 }
 
 export default ProductDatabase;
