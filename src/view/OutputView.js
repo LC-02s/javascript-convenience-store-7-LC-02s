a62edab82ee25d7c6c746a0d2e0b5ccbef2f21c1
ChanLee_KR <cl700120@gmail.com>
2024-11-11 00:21:31 +0900

    feat: 영수증 내 상품 출력 기능 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,9 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
-import { formatKRW } from '../utils/index.js';
+import { formatKRW, padLeft, padRight } from '../utils/index.js';
 
 /** @typedef {import('../types/index.js').Product} Product */
 
 class OutputView {
+  static #RECEIPT_TITLE = `===========W 편의점=============`;
+  static #RECEIPT_GIFT_TITLE = `===========증	 정=============`;
+  static #RECEIPT_TOTAL_TITLE = `==============================`;
+
+  static #RECEIPT_PADDING = Object.freeze({
+    NAME: 14,
+    QUANTITY: 5,
+    PRICE: 10,
+  });
+
   /** @param {Product[]} productList */
   static printProductAll(productList) {
     OutputView.#printProductGuideComment();
@@ -13,7 +23,7 @@ class OutputView {
   }
 
   static #printProductGuideComment() {
-    Console.print('안녕하세요. W편의점입니다.');
+    Console.print('\n안녕하세요. W편의점입니다.');
     Console.print('현재 보유하고 있는 상품입니다.\n');
   }
 
@@ -24,6 +34,37 @@ class OutputView {
     }
 
     return `- ${name} ${formatKRW(price)}원 ${quantity}개 ${promotion ?? ''}`;
+  }
+
+  /** @param {{ productList: Pick<Product, 'name' | 'quantity' | 'price'>[]; giftList: Pick<Product, 'name' | 'quantity'>[]; }} param */
+  static printReceiptProductList({ productList, giftList }) {
+    Console.print(OutputView.#RECEIPT_TITLE);
+    OutputView.#printReceiptColum();
+    productList.forEach(OutputView.#printReceiptProduct);
+    Console.print(OutputView.#RECEIPT_GIFT_TITLE);
+    giftList.forEach(OutputView.#printReceiptProduct);
+  }
+
+  static #printReceiptColum() {
+    const { NAME, QUANTITY, PRICE } = OutputView.#RECEIPT_PADDING;
+    const printingName = padRight('상품명', NAME);
+    const printingQuantity = padRight('수량', QUANTITY);
+    const printingPrice = padRight('금액', PRICE);
+
+    Console.print(printingName + printingQuantity + printingPrice);
+  }
+
+  /** @param {Pick<Product, 'name' | 'quantity' | 'price'>} product */
+  static #printReceiptProduct({ name, quantity, price }) {
+    const { NAME, QUANTITY, PRICE } = OutputView.#RECEIPT_PADDING;
+    const printing =
+      padRight(name, NAME) + padRight(String(quantity), QUANTITY);
+
+    if (!price) {
+      return Console.print(printing);
+    }
+
+    Console.print(printing + padLeft(formatKRW(price), PRICE));
   }
 }
 
