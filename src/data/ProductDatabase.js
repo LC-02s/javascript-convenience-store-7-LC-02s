3d651814a36c4ec6559355f670d66c2049ec23a7
ChanLee_KR <cl700120@gmail.com>
2024-11-11 18:36:23 +0900

    refactor: 상품 데이터베이스 초기 데이터 파싱 기능 요구사항 미 적용분 반영

--- src/data/ProductDatabase.js
+++ src/data/ProductDatabase.js
@@ -8,8 +8,10 @@ class ProductDatabase {
   #data;
 
   constructor() {
-    const productCSV = readFileSync(DATA_PATH.PRODUCT_CSV);
-    this.#data = parseCSV(productCSV);
+    const initialData = this.#getInitialData();
+    const parsedData = this.#parseInitialData(initialData);
+
+    this.#data = this.#generateId(parsedData);
   }
 
   /** @param {Pick<Product, 'name' | 'quantity'>} product */
@@ -48,6 +50,41 @@ class ProductDatabase {
   /** @param {(data: Product[]) => void} printer */
   printAll(printer) {
     printer(this.#data);
+  }
+
+  /** @returns {{ data: Object<string, unknown>[]; productNameList: string[]; }} */
+  #getInitialData() {
+    const productCSV = readFileSync(DATA_PATH.PRODUCT_CSV);
+    const data = parseCSV(productCSV);
+    const productNameList = data.reduce((nameList, { name }) => {
+      if (nameList.some((n) => n === name)) return nameList;
+
+      return [...nameList, name];
+    }, []);
+
+    return { data, productNameList };
+  }
+
+  /**
+   * @param {{ data: Object<string, unknown>[]; productNameList: string[]; }} param
+   * @returns {Omit<Product, 'id'>[]}
+   */
+  #parseInitialData({ data, productNameList }) {
+    return productNameList.reduce((parsed, name) => {
+      const target = data.filter((product) => product.name === name);
+      const [targetProduct] = target;
+
+      if (target.length !== 1) return [...parsed, ...target];
+
+      if (!targetProduct.promotion) return [...parsed, targetProduct];
+
+      return [...parsed, targetProduct, { ...targetProduct, quantity: 0, promotion: null }];
+    }, []);
+  }
+
+  /** @param {Omit<Product, 'id'>[]} parsedData */
+  #generateId(parsedData) {
+    return parsedData.map((data, id) => ({ ...data, id }));
   }
 }
 
