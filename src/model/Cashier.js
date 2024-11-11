7295fce0d18f4ef06a8f2d77c23f3ad190f002c2
ChanLee_KR <cl700120@gmail.com>
2024-11-11 19:53:45 +0900

    feat: 증정품 목록 추출 기능 구현

--- src/model/Cashier.js
+++ src/model/Cashier.js
@@ -1,6 +1,10 @@
 import { ProductDatabase } from '../data/index.js';
+import PromotionChecker from './PromotionChecker.js';
 
-/** @typedef {import('../types/index.js').Product} Product */
+/**
+ * @typedef {import('../types/index.js').Product} Product
+ * @typedef {import('../types/index.js').Promotion} Promotion
+ */
 
 class Cashier {
   /** @type {Pick<Product, 'name' | 'quantity'>[]} */
@@ -9,13 +13,18 @@ class Cashier {
   /** @type {ProductDatabase} */
   #productDB;
 
+  /** @type {PromotionChecker} */
+  #checker;
+
   /**
    * @param {Pick<Product, 'name' | 'quantity'>[]} productList
    * @param {ProductDatabase} productDB
+   * @param {PromotionChecker} promotionChecker
    */
-  constructor(productList, productDB) {
+  constructor(productList, productDB, promotionChecker) {
     this.#productList = productList;
     this.#productDB = productDB;
+    this.#checker = promotionChecker;
   }
 
   #computeTotalPurchaseAmount() {
@@ -31,6 +40,52 @@ class Cashier {
     return this.#productList.reduce((total, { quantity }) => {
       return total + quantity;
     }, 0);
+  }
+
+  /** @return {Product[]} */
+  #divideGiftList() {
+    return this.#productList.reduce((giftList, product) => {
+      return this.#divideGift(giftList, product);
+    }, []);
+  }
+
+  /**
+   * @param {Product[]} giftList
+   * @param {Pick<Product, 'name' | 'quantity'>} product
+   */
+  #divideGift(giftList, { name, quantity }) {
+    const target = this.#getProductAndPromotion(name);
+
+    if (!target) return giftList;
+
+    const { product, promotion } = target;
+    const giftCount = this.#computeGiftCount({ ...promotion, quantity });
+
+    if (giftCount < 1) return giftList;
+
+    return [...giftList, { ...product, quantity: giftCount }];
+  }
+
+  /** @param {Promotion['name']} name */
+  #getProductAndPromotion(name) {
+    const product = this.#productDB.findWithPromotionByName({ name });
+
+    if (!product) return null;
+
+    const { promotion: key } = product;
+    const promotion = this.#checker.getValidPromotion(key ?? '');
+
+    if (!promotion) return null;
+
+    return { product, promotion };
+  }
+
+  /** @param {{ buy: number; get: number; quantity: number; }} param */
+  #computeGiftCount({ buy, get, quantity }) {
+    const unit = buy + get;
+    const count = Math.floor(quantity / unit);
+
+    return get * count;
   }
 }
 
